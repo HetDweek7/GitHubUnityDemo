e571722d591ca4fc26530bb3f67756fd0b326b73
shahhet11 <shahhet11@gmail.com>
2020-08-12 17:12:56 +0530

    fdgdfgfdg
    dfgdf

---
+++ Assets/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour
+{
+
+    public Vector3 RotateAmount; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateAmount * Time.deltaTime);
+    }
+}
