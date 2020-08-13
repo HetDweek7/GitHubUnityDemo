752641de8c3db6d5ed48aaf6fa14696d632242f6
HetDweek7 <het@dweekstudios.com>
2020-08-13 09:26:56 +0530

    New Branches Added
    Includes 3 cubes and a rotational Script

---
+++ Assets/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RotateObject : MonoBehaviour
+{
+
+    public Vector3 RotateAmount;  // degrees per second to rotate in each axis. Set in inspector.
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateAmount * Time.deltaTime);
+    }
+}
